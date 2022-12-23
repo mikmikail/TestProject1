8dd4b91e2f00febba1f12052f62647d3207d2f40
Mikail HAN <mikailhan92@gmail.com>
2022-12-23 14:53:25 -0500

    InnerpackageClass2 and Package1Class 2 modified

--- src/main/java/org/example/package1/innerPackage/InnerpackageClass2.java
+++ src/main/java/org/example/package1/innerPackage/InnerpackageClass2.java
@@ -5,7 +5,8 @@ public class InnerpackageClass2 {
         System.out.println("This is Inner Package Class 2 ");
         int i = 0 ;
         while (i<10){
-            System.out.println("Example package Class 2 While loop");
+            System.out.println("Innerpackage Class 2 While loop");
+            System.out.println("Change for third commit");
             i++;
         }
     }
--- src/main/java/org/example/package1/package1Class2.java
+++ src/main/java/org/example/package1/package1Class2.java
@@ -3,5 +3,11 @@ package org.example.package1;
 public class package1Class2 {
     public static void main(String[] args) {
         System.out.println("This is Package 1 Class 2 ");
+        int i = 0 ;
+        while (i<10){
+            System.out.println("Package1 Class 2 While loop");
+            System.out.println("For third Commit");
+            i++;
+        }
     }
 }
