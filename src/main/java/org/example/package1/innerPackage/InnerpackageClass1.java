3bf8891462833e956ff24bd837728135b85cc46b
Mikail HAN <mikailhan92@gmail.com>
2022-12-23 14:54:47 -0500

    InnerpackageClass1 added

---
+++ src/main/java/org/example/package1/innerPackage/InnerpackageClass1.java
@@ -0,0 +1,7 @@
+package org.example.package1.innerPackage;
+
+public class InnerpackageClass1 {
+    public static void main(String[] args) {
+    System.out.println("This is Inner Package Class 1 ");
+}
+}
