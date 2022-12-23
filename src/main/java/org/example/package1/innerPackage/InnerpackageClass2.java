a79d136d5565e278630539becc0b6fe624625817
Mikail HAN <mikailhan92@gmail.com>
2022-12-23 14:30:26 -0500

    ExampleClass2 and InnerPackageClass2 modified.

--- src/main/java/org/example/package1/innerPackage/InnerpackageClass2.java
+++ src/main/java/org/example/package1/innerPackage/InnerpackageClass2.java
@@ -3,5 +3,10 @@ package org.example.package1.innerPackage;
 public class InnerpackageClass2 {
     public static void main(String[] args) {
         System.out.println("This is Inner Package Class 2 ");
+        int i = 0 ;
+        while (i<10){
+            System.out.println("Example package Class 2 While loop");
+            i++;
+        }
     }
 }
